b5ce20d2a83bc0f0486a1f0c339b619d77d44c11
Soumya Jindal <jindalsoumya@yahoo.com>
2018-11-07 15:56:39 +0530

    Implemented basic V1

---
+++ V1/serialPrintData/serialPrintData.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+Adafruit_INA219 ina219;
+
+void setup() 
+{
+  Serial.begin(115200);
+  while (!Serial) {
+      delay(1);
+  }
+  uint32_t currentFrequency;
+  ina219.begin();
+  ina219.setCalibration_16V_400mA();
+}
+
+void loop() 
+{
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+  Serial.println("");
+
+  delay(2000);
+}
